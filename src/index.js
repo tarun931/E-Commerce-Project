97b763eedca36a6de75c4b446f62fb8c5a5e1b66
tarun931 <75002007+tarun931@users.noreply.github.com>
2024-07-29 02:41:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,24 +7,7 @@ import store from "./store/index";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-// context
-// global state
-// on every change in context all components were getting rerender
 
-// redux
-// state management tool
-// global state
-// in order for the components to be changed based on globacl state
-
-// Store, actions, reducers
-
-// store -  GLobal state
-// reduces - functions which tell how to change state
-// actions - triggers to call these changes
-
-// var a = document.createElement("div")
-// a.innerHTML = "hello sandbox"
-// rootElement.appendChild(a);
 root.render(
   <BrowserRouter>
     <Provider store={store}>
@@ -33,10 +16,3 @@ root.render(
   </BrowserRouter>
 );
 
-// //root.render(    <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>);
-
-// import to get a js function from another file
-// export the js function for it to be imported
